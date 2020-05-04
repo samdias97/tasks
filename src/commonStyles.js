638ace2099dc88f3e7a88590ac1d2b4bd60da543
samdias97 <48606334+samdias97@users.noreply.github.com>
2020-05-04 15:59:41 -0300

    Componente Tarefa #05

--- src/commonStyles.js
+++ src/commonStyles.js
@@ -3,6 +3,6 @@ export default {
     colors: {
         secondary: '#FFF',
         mainText: '#222',
-        
+        subText: '#555',
     }
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -2,12 +2,19 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
 import commonStyles from '../commonStyles'
 
 export default props => {
 
     const doneOrNotStyle = props.doneAt != null ? 
         { textDecorationLine: 'line-through' } : {}
+
+    const date = props.doneAt ? props.doneAt : props.estimateAt
+    const formattedDate = moment(date).locale('pt-br')
+        .format('ddd, D [de] MMMM')
 
     return (
         <View style={styles.container}>
@@ -16,7 +23,7 @@ export default props => {
             </View>
             <View>
                 <Text style={styles.desc, doneOrNotStyle}>{props.desc}</Text>
-                <Text>{props.estimateAt + ""}</Text>
+                <Text style={styles.date}>{formattedDate}</Text>
             </View>
         </View>
     )
@@ -70,5 +77,11 @@ const styles = StyleSheet.create({
         fontFamily: commonStyles.fontFamily,
         color: commonStyles.colors.mainText,
         fontSize: 15,
+    },
+    date: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.subText,
+        fontSize: 12,
+
     }
 })
