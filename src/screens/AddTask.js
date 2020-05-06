1ffe16f71e419932ee48a95150a588be0fdbf633
samdias97 <48606334+samdias97@users.noreply.github.com>
2020-05-06 14:29:58 -0300

    Modal Adicionar Tarefa #03

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react'
-import { Modal, View, StyleSheet, TouchableWithoutFeedback, TouchableWithoutFeedbackBase, Vibration } from 'react-native'
+import { 
+    Modal, 
+    View, 
+    Text,
+    StyleSheet,
+    TouchableWithoutFeedback,
+    TouchableOpacity,
+    TextInput,
+} from 'react-native'
 
+import commonStyles from '../commonStyles'
+
+const initialState = { desc: '' }
 
 export default class AddTask extends Component {
+
+    state = {
+        ...initialState
+    }
+
     render() {
         return (
             <Modal 
@@ -13,6 +29,25 @@ export default class AddTask extends Component {
                 <TouchableWithoutFeedback onPress={this.props.onCancel}>
                     <View style={styles.background}></View>
                 </TouchableWithoutFeedback>
+                <View style={styles.container}>
+                    <Text style={styles.header}>Nova Tarefa</Text>
+                    <TextInput style={styles.input}
+                        placeholder="Informe a Descrição..."
+                        value={this.state.desc}
+                        onChangeText={desc => this.setState({ desc })}></TextInput>
+                    <View style={styles.buttons}>
+                        <TouchableOpacity onPress={this.props.onCancel}>
+                            <Text style={styles.button}>Cancelar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Text style={styles.button}>Salvar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <TouchableWithoutFeedback onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
+
             </Modal>
         )   
     }
@@ -22,6 +57,35 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
         backgroundColor: 'rgba(0, 0, 0, 0.7)',
-
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF'
+    },
+    header: {
+        fontFamily: commonStyles.fontFamily,
+        backgroundColor: commonStyles.colors.today,
+        color: commonStyles.colors.secondary,
+        textAlign: "center",
+        padding: 15,
+        fontSize: 18,
+    },
+    input: {
+        fontFamily: commonStyles.fontFamily,
+        height: 40,
+        margin: 15,
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#E3E3E3',
+        borderRadius: 6
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+    button: {
+        margin: 20,
+        marginRight: 30,
+        color: commonStyles.colors.today
     }
 })
