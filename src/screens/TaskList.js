f2d5cd98b1b0588f2084d72da24f465e6a41bfca
samdias97 <48606334+samdias97@users.noreply.github.com>
2020-05-06 12:19:23 -0300

    Botão Filtrar Tarefas

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Platform } from 'react-native'
 import { Container, ImageBackground, List, TitleBar } from '../styles'
 
 import commonStyles from '../commonStyles'
 import todayImage from '../../assets/imgs/today.jpg'
+
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 import moment from 'moment'
 import 'moment/locale/pt-br'
@@ -11,8 +13,9 @@ import 'moment/locale/pt-br'
 import Task from '../components/Task'
 
 export default class TaskList extends Component {
-
     state = {
+        showDoneTasks: true,
+
         tasks: [{
             id: Math.random(),
             desc: 'Comprar Livro de React Native',
@@ -25,7 +28,10 @@ export default class TaskList extends Component {
             doneAt: null,
         }]
     }
-    
+    toggleFilter = () => {
+        this.setState({ showDoneTasks: !this.state.showDoneTasks })
+    }
+
     toggleTask = taskId => {
         const tasks = [...this.state.tasks]
         tasks.forEach(task => {
@@ -43,6 +49,12 @@ export default class TaskList extends Component {
         return (
         <Container>
             <ImageBackground source={todayImage}>
+                <View style={styles.iconBar}>
+                    <TouchableOpacity onPress={this.toggleFilter}>
+                        <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
+                            size={20} color={commonStyles.colors.secondary} />
+                    </TouchableOpacity>
+                </View>
                 <TitleBar>
                     <Text style={styles.title}>Hoje</Text>
                     <Text style={styles.subtitle}>{today}</Text>
@@ -72,5 +84,11 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginLeft: 20,
         marginBottom: 30
+    },
+    iconBar: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        justifyContent: 'flex-end',
+        marginTop: Platform.OS === 'ios' ? 40 : 10
     }
 })
